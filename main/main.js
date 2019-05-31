5c03f6f7921f8d115f923537bdf508a0a60b97b0
sdh <251287012@qq.com>
2019-05-31 18:07:33 +0800

    ok

--- main/main.js
+++ main/main.js
@@ -1,4 +1,42 @@
-module.exports = function main() {
+module.exports = function main(num) {
     console.log("Debug Info");
-    return 'Hello World!';
+	var strs = [];
+	for(var j=0;j<num.length;++j){
+		var ch = num.charAt(j);
+		strs.push(getStrFromNum(ch));
+	}
+	var ans = ''; 
+    for(var line=0;line<3;++line){
+		for(var i=0;i<strs.length;++i){
+			ans += strs[i][line];
+			if(i==strs.length-1)
+				ans += '\n';
+			else
+				ans += ' ';
+		}
+	}
+	return ans;
 };
+
+function getStrFromNum(num) {
+	if(num==0)
+		return ['._.','|.|','|_|'];
+	else if(num ==1)
+		return ['...','..|','..|'];
+	else if(num ==2)
+		return ['._.','._|','|_.'];
+	else if(num ==3)
+		return ['._.','._|','._|'];
+	else if(num ==4)
+		return ['...','|_|','..|'];
+	else if(num ==5)
+		return ['._.','|_.','._|'];
+	else if(num ==6)
+		return ['._.','|_.','|_|'];
+	else if(num ==7)
+		return ['._.','..|','..|'];
+	else if(num ==8)
+		return ['._.','|_|','|_|'];
+	else
+		return ['._.','|_|','..|'];
+}
